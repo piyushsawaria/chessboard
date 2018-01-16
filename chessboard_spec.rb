206b1fb3f0e2a3ab5d1f9297b18a13579d26bd53
Piyush Sawaria <piyush.sawaria@trantorinc.com>
2018-01-16 22:11:54 +0530

    added delete feature and test cases

---
+++ chessboard_spec.rb
@@ -0,0 +1,67 @@
+$LOAD_PATH << '.'
+require 'chessboard'
+
+describe 'chessboard' do
+	context '#all_possible_moves' do
+  	it 'return all possible cordinates from given cordinates' do
+  		possiblities = ChessBoard.new.all_possible_moves('knight', [0,0])
+  		expect(possiblities).to eq [[2,1], [1,2]]
+  	end
+  end
+
+  context '#convert_position_to_coordinates' do    
+    it 'return the cordinate of a given position' do 
+      cordinate = ChessBoard.new.convert_position_to_coordinates('a1')
+      expect(cordinate).to eq [0,0]
+    end    
+  end
+
+  context '#convert_coordinates_to_position' do
+  	it 'return the position from given array of cordinates' do
+  		positions = ChessBoard.new.convert_coordinates_to_position([[0,0],[1,1],[2,2]])
+  		expect(positions).to eq 'a1,b2,c3'
+  	end
+  end
+
+  context '#show_possible_moves' do
+  	it 'return possible moves in string' do
+  		moves = ChessBoard.new.show_possible_moves('Knight', 'a1')
+  		expect(moves).to eq 'c2,b3'
+  	end
+  end
+
+  context '#moveset' do
+  	it 'return array of first possible coordinates' do
+  		possible_arr = ChessBoard.new.moveset('rook')
+  		expect(possible_arr).to eq [[0, 1],[0, -1],[1, 0],[-1, 0]]
+  	end
+  end
+
+  context '#is_valide_input?' do
+  	it 'return true' do
+  		val = ChessBoard.new.is_valide_input?('queen','d4')
+  		expect(val).to eq true
+  	end
+  	it 'return false' do
+  		val = ChessBoard.new.is_valide_input?('queen','d9')
+  		expect(val).to eq false
+  	end
+  end
+end
+
+describe 'ShortestPath' do
+	context '#find_longest_distance_piece' do
+		it 'return target coordinates and position' do
+			pos2_cor, pos2_text = ShortestPath.new.find_longest_distance_piece([0,0],'a1,h7,g5')
+			expect(pos2_cor).to eq [7,6]
+			expect(pos2_text).to eq 'h7'
+		end
+	end
+	
+	context '#piece_movement' do
+		it 'shows the shortest route taken' do
+			route = ShortestPath.new.piece_movement('knight','a1', 'b4,d2,a5,h4,h2,h8')
+			expect(route).to eq 'a1,c2,a3,c4,e5,g6,h8'
+		end
+	end
+end
--- contactList/app.js
+++ contactList/app.js
@@ -1,59 +1,8 @@
 angular.module('contact', [])
-.controller('Contact', 
-function($scope, $http) {
-  // $http.get('api.json').
-  //   then(function(response) {
-  //     $scope.contactList = response.data;
-  //   });
-  $scope.contactList = {
-		"contacts" : [
-			{
-				"type" : "Executive",
-				"name" : "Ann Brown",
-				"title" : "CEO",
-				"phone" : "(512)456-5555",
-				"ext" : "",
-				"fax" : "(512)456-5555",
-				"email" : "Executive" 
-			},
-			{
-				"type" : "Inmar AR",
-				"name" : "Mary Smith",
-				"title" : "Loren Ipsum",
-				"phone" : "(512)456-5555",
-				"ext" : "",
-				"fax" : "(512)456-5555",
-				"email" : "Inmar AR"
-			},
-			{
-				"type" : "Executive",
-				"name" : "John Doe",
-				"title" : "Dolor Sit",
-				"phone" : "(512)456-5555",
-				"ext" : "",
-				"fax" : "(512)456-5555",
-				"email" : "Executive"
-			},
-			{
-				"type" : "Daily",
-				"name" : "John Doe",
-				"title" : "Dolor sit amet",
-				"phone" : "(512)456-5555",
-				"ext" : "",
-				"fax" : "(512)456-5555",
-				"email" : "Daily"
-			},
-			{
-				"type" : "Other",
-				"name" : "John Doe",
-				"title" : "Loren Ipsum",
-				"phone" : "(512)456-5555",
-				"ext" : "",
-				"fax" : "(512)456-5555",
-				"email" : "Other"
-			}
-		]
-	}
+.controller('Contact', function($scope, $http, $window) {
+  $http.get('api.json').then(function(response) {
+    $scope.contactList = response.data;
+  });
 
   $scope.addContact = function(){
 		$scope.contactList.contacts.push({
@@ -75,6 +24,27 @@ function($scope, $http) {
 	};
 
 	$scope.deleteContact = function(index){
-		$scope.contactList.contacts.splice(index, 1);
+		if ($window.confirm("Do you want to continue?")){
+			$scope.contactList.contacts.splice(index, 1);
+		}
+	};
+
+	$scope.deleteSelectedContact = function(){
+		if ($window.confirm("Do you want to continue?")){
+			var newContactList=[];
+			angular.forEach($scope.contactList.contacts,function(row){
+				if(!row.isChecked){
+					newContactList.push(row);
+				}
+			});
+			$scope.contactList.contacts = newContactList;
+		}
+	};
+
+	$scope.allCheck = function(){
+		var check = $scope.isAllChecked
+		angular.forEach($scope.contactList.contacts,function(row){
+			row.isChecked = check ? true : false;
+		});
 	};
 });
