7805b7ce0ecb946295b30cb81457a5e2fa6dc8a4
Piyush Sawaria <piyush.sawaria@trantorinc.com>
2018-01-17 08:55:29 +0530

    consuming api

--- contactList/app.js
+++ contactList/app.js
@@ -1,58 +1,8 @@
 angular.module('contact', [])
 .controller('Contact', function($scope, $http, $window) {
-  // $http.get('api.json').then(function(response) {
-  //   $scope.contactList = response.data;
-  // });
-
-  $scope.contactList = {
-	"contacts" : [
-		{
-			"type" : "Executive",
-			"name" : "Ann Brown",
-			"title" : "CEO",
-			"phone" : "(512)456-5555",
-			"ext" : "",
-			"fax" : "(512)456-5555",
-			"email" : "Executive" 
-		},
-		{
-			"type" : "Inmar AR",
-			"name" : "Mary Smith",
-			"title" : "Loren Ipsum",
-			"phone" : "(512)456-5555",
-			"ext" : "",
-			"fax" : "(512)456-5555",
-			"email" : "Inmar AR"
-		},
-		{
-			"type" : "Executive",
-			"name" : "John Doe",
-			"title" : "Dolor Sit",
-			"phone" : "(512)456-5555",
-			"ext" : "",
-			"fax" : "(512)456-5555",
-			"email" : "Executive"
-		},
-		{
-			"type" : "Daily",
-			"name" : "John Doe",
-			"title" : "Dolor sit amet",
-			"phone" : "(512)456-5555",
-			"ext" : "",
-			"fax" : "(512)456-5555",
-			"email" : "Daily"
-		},
-		{
-			"type" : "Other",
-			"name" : "John Doe",
-			"title" : "Loren Ipsum",
-			"phone" : "(512)456-5555",
-			"ext" : "",
-			"fax" : "(512)456-5555",
-			"email" : "Other"
-		}
-	]
-}
+  $http.get('api.json').then(function(response) {
+    $scope.contactList = response.data;
+  });
 
   $scope.addContact = function(){
 		$scope.contactList.contacts.push({
