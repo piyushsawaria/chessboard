56ceb02955f37df0a2a55af59b8141ab19305c7b
Piyush Sawaria <piyush.sawaria@trantorinc.com>
2018-01-17 08:51:05 +0530

    added search field

--- contactList/app.js
+++ contactList/app.js
@@ -1,8 +1,58 @@
 angular.module('contact', [])
 .controller('Contact', function($scope, $http, $window) {
-  $http.get('api.json').then(function(response) {
-    $scope.contactList = response.data;
-  });
+  // $http.get('api.json').then(function(response) {
+  //   $scope.contactList = response.data;
+  // });
+
+  $scope.contactList = {
+	"contacts" : [
+		{
+			"type" : "Executive",
+			"name" : "Ann Brown",
+			"title" : "CEO",
+			"phone" : "(512)456-5555",
+			"ext" : "",
+			"fax" : "(512)456-5555",
+			"email" : "Executive" 
+		},
+		{
+			"type" : "Inmar AR",
+			"name" : "Mary Smith",
+			"title" : "Loren Ipsum",
+			"phone" : "(512)456-5555",
+			"ext" : "",
+			"fax" : "(512)456-5555",
+			"email" : "Inmar AR"
+		},
+		{
+			"type" : "Executive",
+			"name" : "John Doe",
+			"title" : "Dolor Sit",
+			"phone" : "(512)456-5555",
+			"ext" : "",
+			"fax" : "(512)456-5555",
+			"email" : "Executive"
+		},
+		{
+			"type" : "Daily",
+			"name" : "John Doe",
+			"title" : "Dolor sit amet",
+			"phone" : "(512)456-5555",
+			"ext" : "",
+			"fax" : "(512)456-5555",
+			"email" : "Daily"
+		},
+		{
+			"type" : "Other",
+			"name" : "John Doe",
+			"title" : "Loren Ipsum",
+			"phone" : "(512)456-5555",
+			"ext" : "",
+			"fax" : "(512)456-5555",
+			"email" : "Other"
+		}
+	]
+}
 
   $scope.addContact = function(){
 		$scope.contactList.contacts.push({
@@ -46,5 +96,17 @@ angular.module('contact', [])
 		angular.forEach($scope.contactList.contacts,function(row){
 			row.isChecked = check ? true : false;
 		});
+	};
+
+	$scope.search = function(item){
+		if ($scope.searchContact == undefined) {
+			return true;
+		}
+		else{
+			if(item.name.toLowerCase().indexOf($scope.searchContact.toLowerCase()) != -1 || item.email.toLowerCase().indexOf($scope.searchContact.toLowerCase()) != -1){
+				return true;
+			}
+		}
+		return false;
 	};
 });
